2d1888bd20192cda8175300b5459c815997a3a92
Vishal Deshmukh <vishaldeshmuk143@gmail.com>
2025-07-04 10:24:10 +0530

    add Backend code and css animation

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import requests
+from urllib.parse import quote  
+
+app = Flask(__name__)
+
+API_KEY = '85101a50cd01f77e4f261a5039778fe9' 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form['city'].strip()
+        city_encoded = quote(city)
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city_encoded}&appid={API_KEY}&units=metric'
+
+        print(" URL:", url)
+        response = requests.get(url)
+        print(" Status Code:", response.status_code)
+        print(" Response:", response.text)
+
+        if response.status_code == 200:
+            data = response.json()
+            weather_data = {
+                'city': data['name'],
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'].title(),
+                'icon': data['weather'][0]['icon']
+            }
+        else:
+            try:
+                error_message = response.json().get('message', 'City not found')
+            except:
+                error_message = 'City not found'
+            weather_data = {'error': error_message}
+
+    return render_template('index.html', weather=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
